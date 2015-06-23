96389b53b2953d1c73ed2186a8a02d9821bcb680
DavidRodriguezB <david.rodriguezbolois@gmail.com>
2015-06-23 16:57:12 +0100

    New test added

--- src/com/innerfunction/uri/test/CompoundURITest.java
+++ src/com/innerfunction/uri/test/CompoundURITest.java
@@ -1,37 +1,66 @@
 package com.innerfunction.uri.test;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.net.URISyntaxException;
 
 import com.innerfunction.uri.CompoundURI;
 
 import android.test.AndroidTestCase;
-import android.util.Log;
 
 public class CompoundURITest extends AndroidTestCase {
 
-    String uri="s:name+p1@[s:p2+np@[bang:x+p@3]]+p2@s:p2";
-    String uri2="s:name+p3[s:p2]";
+    String uri="s:name#hue+p1@[s:p2+np@[bang:x+p@3]]+p2@s:p2";
+    String urifail="hueheuheuheuheu";
     CompoundURI cpu;
-    Map<String,CompoundURI> params=new HashMap<String,CompoundURI>();;
-    
+    String Scheme, Name, Fragment;
+
     public void setUp() throws Exception {
         cpu = new CompoundURI(uri);
     }
     
-    public void testGenerics(){ 
-        params.put(uri2, cpu);
-
-        String Scheme,Name;
+    public void testGetScheme(){ 
         Scheme=cpu.getScheme();
-        Name=cpu.getName();
         assertEquals("s", Scheme);
+    }
+    
+    public void testGetName(){ 
+        Name=cpu.getName();
         assertEquals("name", Name);
+    }
+    
+    public void testGetFragment(){ 
+        Fragment=cpu.getFragment();
+        assertEquals("hue", Fragment);
+    }
+    
+    public void testCopyOf(){
+        assertEquals( cpu, cpu.copyOf());
+        assertEquals( "s:name#hue.hue+p1@[s:p2+np@[bang:x+p@[s:3]]]+p2@[s:p2]" , cpu.copyOfWithFragment("hue").toString());
+    }
+    
+    public void testGetParameters(){
         assertEquals("s:p2+np@[bang:x+p@[s:3]]", cpu.getParameter("p1").toString());
         assertEquals("s:p2", cpu.getParameter("p2").toString());
-        cpu.addParameters(params);
-        Log.i("hue", cpu.getParameter("p1").toString());
-        Log.i("hue", cpu.getParameter("p2").toString());
-
+        assertEquals(2,cpu.getParameters().size());
+    }
+    
+    public void testCanonicalForm(){
+        assertEquals("s:name#hue+p1@[s:p2+np@[bang:x+p@[s:3]]]+p2@[s:p2]",cpu.canonicalForm());
+    }
+    
+    public void testToString(){
+        assertEquals("s:name#hue+p1@[s:p2+np@[bang:x+p@[s:3]]]+p2@[s:p2]",cpu.toString());
+    }
+    
+    public void testHashCode(){
+        assertNotNull( cpu.hashCode());
+    }
+    
+    public void testException(){
+        urifail="lakdfadufhnuiaiuansdu";
+        try {
+            cpu = new CompoundURI(urifail);
+            } catch (URISyntaxException e) {
+            assertTrue(true);
+        }
     }
 }
