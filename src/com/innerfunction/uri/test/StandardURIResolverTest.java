c6f2e16b9056b55c4ab6ec0046dd12b19d36f4ed
Julian Goacher <julian.goacher@innerfunction.com>
2015-12-11 19:27:16 +0000

    Fixed unit test.

--- src/com/innerfunction/uri/test/StandardURIResolverTest.java
+++ src/com/innerfunction/uri/test/StandardURIResolverTest.java
@@ -44,8 +44,8 @@ public class StandardURIResolverTest extends AndroidTestCase {
         edit.putString("email", "info@innerfunction.com");
         edit.putString("name", "InnerFunction");
         edit.commit();
-        assertEquals(prefs.getString("email", ""),sur.resolveURIFromString("local:email").toString());
-        assertEquals(prefs.getString("name", ""),sur.resolveURIFromString("local:name").toString());
+        assertEquals(prefs.getString("email", ""),sur.dereference("local:email").toString());
+        assertEquals(prefs.getString("name", ""),sur.dereference("local:name").toString());
     }
     
     public void testCache() throws IOException, URISyntaxException{
@@ -53,21 +53,21 @@ public class StandardURIResolverTest extends AndroidTestCase {
         File outputFile = new File(outputDir);
         outputFile.createNewFile();
         FileIO.writeString( outputFile , "This is the temporary file content" );
-        assertEquals("cache:%2Ftemp.tmp",sur.resolveURIFromString("cache:/temp.tmp").getURI().toString());
-        assertEquals("cache", sur.resolveURIFromString("cache:/temp.tmp").getURI().getScheme());
-        assertEquals("This is the temporary file content", sur.resolveURIFromString("cache:/temp.tmp").toString());
-        assertEquals("/storage/emulated/0/Android/data/com.innerfunction.semo/files/Download/temp.tmp", sur.resolveURIFromString("cache:/temp.tmp").asDefault().toString());
+        assertEquals("cache:%2Ftemp.tmp",sur.dereference("cache:/temp.tmp").getURI().toString());
+        assertEquals("cache", sur.dereference("cache:/temp.tmp").getURI().getScheme());
+        assertEquals("This is the temporary file content", sur.dereference("cache:/temp.tmp").toString());
+        assertEquals("/storage/emulated/0/Android/data/com.innerfunction.semo/files/Download/temp.tmp", sur.dereference("cache:/temp.tmp").asDefault().toString());
         outputFile.delete();
     }
     
     public void testApp() throws IOException{
-        assertNotNull(sur.resolveURIFromString("app:/configuration.json"));
-        assertEquals("configuration.json", sur.resolveURIFromString("app:/configuration.json").asDefault().toString());
-        assertEquals(FileIO.readString(getContext().getAssets().open("configuration.json"),"json" ,"UTF-8"),sur.resolveURIFromString("app:/configuration.json").toString());
+        assertNotNull(sur.dereference("app:/configuration.json"));
+        assertEquals("configuration.json", sur.dereference("app:/configuration.json").asDefault().toString());
+        assertEquals(FileIO.readString(getContext().getAssets().open("configuration.json"),"json" ,"UTF-8"),sur.dereference("app:/configuration.json").toString());
     }
     
     public void tests(){
-        assertEquals("name",sur.resolveURIFromString(uri).toString());
-        assertEquals("s",sur.resolveURIFromString(uri).getURI().getScheme());
+        assertEquals("name",sur.dereference(uri).toString());
+        assertEquals("s",sur.dereference(uri).getURI().getScheme());
     }
 }
